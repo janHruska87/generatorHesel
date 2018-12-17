f733b66f4edaf9c5dc80d4c8ec3afe1ded89d752
janHruska87 <jan.hruska87@seznam.cz>
2018-12-17 10:25:29 +0100

    Hotove

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,13 @@
 package sample;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,14 +25,33 @@ public class Controller implements Initializable{
     @FXML
     private Button bt_konec;
     @FXML
-    private ComboBox cb_pocetZnaku;
+    private Spinner<Integer> sp_pocetZnaku;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-            ObservableList<String> znaky = FXCollections.observableArrayList("1","2","3");
-            cb_pocetZnaku.setItems(znaky);
+        SpinnerValueFactory<Integer> spValue = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
+        sp_pocetZnaku.setValueFactory(spValue);
+        tf_vstupniZnaky.setText("ABCDEFGHIJKLMNOPQabcdefgijklmnopq123456789");
+    }
 
+
+    public void getHeslo(ActionEvent actionEvent) {
+        Heslo psw = new Heslo(sp_pocetZnaku.getValue(),tf_vstupniZnaky.getText());
+        tf_vygenerovaneHeslo.setText(psw.getHeslo());
+
+    }
+
+    public void setClear(ActionEvent actionEvent) {
+        tf_vygenerovaneHeslo.setText("");
+        tf_vstupniZnaky.setText("");
+        SpinnerValueFactory<Integer> spValue = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,100,1);
+        sp_pocetZnaku.setValueFactory(spValue);
+    }
+
+    public void closeWindow(ActionEvent actionEvent) {
+        Stage stage = (Stage) bt_konec.getScene().getWindow();
+        stage.close();
     }
 }
 
--- src/sample/Heslo.java
+++ src/sample/Heslo.java
@@ -4,26 +4,44 @@ import java.util.Random;
 
 public class Heslo {
 
-    private String Heslo;
+    private String heslo;
     private int znaku;
     private String mozneZnaky;
 
-    public Heslo(int znaku, String mozneZnaky) {
+    private void setHeslo(String heslo) {
+        this.heslo = heslo;
+    }
+
+    private int getZnaku() {
+        return znaku;
+    }
+
+    private void setZnaku(int znaku) {
         this.znaku = znaku;
+    }
+
+    private String getMozneZnaky() {
+        return mozneZnaky;
+    }
+
+    private void setMozneZnaky(String mozneZnaky) {
         this.mozneZnaky = mozneZnaky;
+    }
+
+    public Heslo(int znaku, String mozneZnaky) {
+        setZnaku(znaku);
+        setMozneZnaky(mozneZnaky);
     }
 
     public String getHeslo()
     {
         String heslo = "";
-        for (int i = 1; i<this.znaku;i++) {
-            Random rn = new Random();
-            int znakNaPozici = rn.nextInt(mozneZnaky.length());
-            System.out.println("Vygenerovano.." + mozneZnaky.indexOf()znakNaPozici);
-
-
+        Random rn = new Random();
+        for (int i = 1; i<=getZnaku();i++) {
+            int pozice = rn.nextInt(getMozneZnaky().length());
+            heslo += getMozneZnaky().charAt(pozice);
         }
-        return "test";
+        return heslo;
     }
 
 }
